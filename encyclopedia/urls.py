87b6433e7960338ddc57fb1e0fbd40560b4f8b76
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-17 23:32:01 +0300

    Rename entries route to wiki.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('entries/<str:entry>', views.entry, name='title'),
+    path('wiki/<str:entry>', views.entry, name='title'),
     path('search', views.search, name='search'),
 ]
