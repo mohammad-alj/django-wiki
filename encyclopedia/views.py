c83356992e2a1fcac06ffee8a90ff6c91d0e5332
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-17 22:28:18 +0300

    Create url to get the entry content.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .util import get_entry
+
 
 from . import util
 
@@ -7,4 +10,8 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(req: HttpRequest, entry: str):
+    return render(req, 'encyclopedia/entry.html', {'entry_name': entry, 'entry_content': get_entry(entry)})
 
