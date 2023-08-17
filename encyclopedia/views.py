fdaade0f85cfc2e1f2e52e4c5a006258046a18f8
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-17 23:52:34 +0300

    Refactor: Change the error template.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -15,7 +15,7 @@ def index(request):
 def entry(req: HttpRequest, entry: str):
     content = util.get_entry(entry)
     if not content:
-        return render(req, 'encyclopedia/error.html', {'error_message': '404 entry not found'})
+        return render(req, 'encyclopedia/error.html', {'code': 404, 'message': 'entry not found.'})
     return render(req, 'encyclopedia/entry.html', {'entry_name': entry, 'entry_content': markdown(util.get_entry(entry))})
 
 
@@ -23,7 +23,7 @@ def search(req: HttpRequest):
     if req.method == 'GET':
         q = req.GET['q']
         if not q or len(q) > 75:
-            return render(req, 'encyclopedia/error.html', {'error_message': '403: Query must be atleast 75 characters long.'})
+            return render(req, 'encyclopedia/error.html', {'code': 403, 'message': 'Query must be atleast 75 characters long.'})
         
         entries = util.list_entries()
         simular_entries = []
@@ -42,4 +42,4 @@ def search(req: HttpRequest):
             return render(req, 'encyclopedia/search_results.html', {'entries': simular_entries})
         else:
             # nothing found...
-            return render(req, 'encyclopedia/error.html', {'error_message': '404: Entry not found.'})
+            return render(req, 'encyclopedia/error.html', {'code': 404, 'message': 'entry not found.'})
